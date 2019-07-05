b0722729ea1dd59d7baa942e5d9e0b393a002cf3
Jongchan Park <jongchan@kaist.ac.kr>
2019-07-05 01:08:16 +0000

    training script modified

--- imagenet/scripts/run_baseline_resnet18.sh
+++ imagenet/scripts/run_baseline_resnet18.sh
@@ -1,32 +1,27 @@
 
+# Settings from S4L paper. 200 epochs, base LR 0.1, LR decay at 140, 160, 180. Batch size unknown. weight decay 0.001
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
     python train_imagenet.py ./ImageNet/ \
     --arch resnet18 \
     --workers 20 \
     --batch-size 256 \
     --lr 0.1 \
-    >> logs/baseline_resnet18_bs256.log &&
+    --weight-decay 0.001 \
+    --max-iter 100000 \
+    --lr-drop-iter 70000 80000 90000 \
+    --warmup --warmup-iter 2500 \
+    --save_dir checkpoint/baseline_resnet18_S4L_bs256 \
+    2>&1 | tee logs/baseline_resnet18_S4L_bs256 &
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 \
     python train_imagenet.py ./ImageNet/ \
     --arch resnet18 \
     --workers 20 \
-    --batch-size 128 \
-    --lr 0.05 \
-    >> logs/baseline_resnet18_bs128.log &&
-
-CUDA_VISIBLE_DEVICES=0,1 \
-    python train_imagenet.py ./ImageNet/ \
-    --arch resnet18 \
-    --workers 20 \
-    --batch-size 64 \
-    --lr 0.025 \
-    >> logs/baseline_resnet18_bs64.log &
-
-CUDA_VISIBLE_DEVICES=2,3 \
-    python train_imagenet.py ./ImageNet/ \
-    --arch resnet18 \
-    --workers 20 \
-    --batch-size 32 \
-    --lr 0.0125 \
-    >> logs/baseline_resnet18_bs32.log 
+    --batch-size 512 \
+    --lr 0.2 \
+    --weight-decay 0.001 \
+    --max-iter 100000 \
+    --lr-drop-iter 70000 80000 90000 \
+    --warmup --warmup-iter 2500 \
+    --save_dir checkpoint/baseline_resnet18_S4L_bs512 \
+    2>&1 | tee logs/baseline_resnet18_S4L_bs512 &
--- imagenet/scripts/run_baseline_resnet50.sh
+++ imagenet/scripts/run_baseline_resnet50.sh
@@ -1,55 +1,40 @@
-#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-#    python train_imagenet.py ./ImageNet/ \
-#    --arch resnet50 \
-#    --workers 20 \
-#    --batch-size 512 \
-#    --lr 0.1 \
-#    --save_dir checkpoint/baseline_resnet50_bs512_lr_0.1
-
-#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-#    python train_imagenet.py ./ImageNet/ \
-#    --arch resnet50 \
-#    --workers 20 \
-#    --batch-size 512 \
-#    --lr 0.2 \
-#    --save_dir checkpoint/baseline_resnet50_bs512_lr_0.2
 
 # Settings from S4L paper. 200 epochs, base LR 0.1, LR decay at 140, 160, 180. Batch size unknown. weight decay 0.001
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-    python train_imagenet.py ./ImageNet/ \
-    --arch resnet50 \
-    --workers 20 \
-    --batch-size 256 \
-    --lr 0.1 \
-    --weight-decay 0.001 \
-    --max-iter 100000 \
-    --lr-drop-iter 70000 80000 90000 \
-    --warmup --warmup-iter 2500 \
-    --save_dir checkpoint/baseline_resnet50_S4L_bs256 \
-    >> logs/baseline_resnet50_S4L_bs256
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+#    python train_imagenet.py ./ImageNet/ \
+#    --arch resnet50 \
+#    --workers 20 \
+#    --batch-size 256 \
+#    --lr 0.1 \
+#    --weight-decay 0.001 \
+#    --max-iter 100000 \
+#    --lr-drop-iter 70000 80000 90000 \
+#    --warmup --warmup-iter 2500 \
+#    --save_dir checkpoint/baseline_resnet50_S4L_bs256 \
+#    >> logs/baseline_resnet50_S4L_bs256
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python train_imagenet.py ./ImageNet/ \
     --arch resnet50 \
-    --workers 20 \
+    --workers 40 \
     --batch-size 512 \
-    --lr 0.1 \
+    --lr 0.2 \
     --weight-decay 0.001 \
     --max-iter 100000 \
     --lr-drop-iter 70000 80000 90000 \
     --warmup --warmup-iter 2500 \
     --save_dir checkpoint/baseline_resnet50_S4L_bs512 \
-    >> logs/baseline_resnet50_S4L_bs512
+    2>&1 | tee logs/baseline_resnet50_S4L_bs512
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python train_imagenet.py ./ImageNet/ \
     --arch resnet50 \
-    --workers 20 \
+    --workers 40 \
     --batch-size 512 \
-    --lr 0.1 \
+    --lr 0.2 \
     --weight-decay 0.001 \
     --max-iter 50000 \
     --lr-drop-iter 35000 40000 45000 \
     --warmup --warmup-iter 2500 \
     --save_dir checkpoint/baseline_resnet50_S4L_bs512_50K \
-    >> logs/baseline_resnet50_S4L_bs512_50K
+    2>&1 | tee logs/baseline_resnet50_S4L_bs512_50K
